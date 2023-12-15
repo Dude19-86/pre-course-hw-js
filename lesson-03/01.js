f036a8e679781ca18585d1c08ed340518818cb34
mrcruel <mrcrueloneman@gmail.com>
2023-12-16 01:18:41 +0300

    homework 3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,7 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(e => e === 'черный' || e === 'красный' || e === 'желтый' ? e : null).join('-');
+}
+
+console.log(createColorString());
