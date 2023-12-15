b03f6c5de2b2b2a5310780f4b62d0007105bfc45
mrcruel <mrcrueloneman@gmail.com>
2023-12-16 01:36:22 +0300

    homework 3

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,1 +1,7 @@
-// code
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+
+function findOrange(arr) {
+    console.log(arr.indexOf('апельсин') > -1 ? 'Ура! нашел' : 'Придется поискать в другом магазине…');
+}
+
+findOrange(words);
