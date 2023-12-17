f792be8b4f923efd20e921663844a734724416ee
mrcruel <mrcrueloneman@gmail.com>
2023-12-17 20:47:03 +0300

    homework 3

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -1,1 +1,9 @@
-// code
+const numbers = [
+    [1, 2, 3, 4, 5],
+    [11, 20, 33, 40, 55],
+    [111, 200, 333, 400, 555],
+];
+
+let res = numbers.filter((e, i) => {
+    console.log(e.filter(c => c % 2 === 0))
+});
